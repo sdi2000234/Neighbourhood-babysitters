22cb6cdf9f5b837559ccd8d0a87fd136a7120d4d
ChristinaKok <christinakokk01@gmail.com>
2024-12-07 22:30:52 +0200

    Initial Home Page :)

---
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './HomePage.css'
+
+const HomePage = () => {
+  return (
+    <>
+    
+    <div>
+        This is the home page :)
+    </div>
+    
+    
+    </>
+  )
+}
+
+export default HomePage;

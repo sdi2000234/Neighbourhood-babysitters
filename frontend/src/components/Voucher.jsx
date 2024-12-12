23a089b46ed5e22a7a02ec0e2ca03ca78751830a
sdi2000166 <sdi2000166@di.uoa.gr>
2024-12-12 22:18:08 +0200

    adjustments

--- frontend/src/components/Voucher.jsx
+++ frontend/src/components/Voucher.jsx
@@ -3,6 +3,7 @@ import './Voucher.css'
 import checkMark from '../assets/checkmark.png'
 import crossMark from '../assets/crossmark.png'
 import defaultQR from '../assets/QRCode.jpeg'
+import epmtyProfile from '../assets/empty_profile.png'
 
 function Voucher()
 {
@@ -21,26 +22,26 @@ function Voucher()
         <div>
     		<div className="payPannel">
                 <div className="userInfo">
-                    <img className="userPfp" src={professionalPfp}/>
-                    <p>{professionalFirstName} {professionalLastName}</p>
+                    <img className="userPfp" src={professionalPfp===null ? epmtyProfile : professionalPfp}/>
+                    <p>{professionalFirstName===null ? "Όνομα" : professionalFirstName} {professionalLastName===null ? "Επώνυμο" : professionalFirstName}</p>
                 </div>
                 <div className="payDetails">
-                    <p><b>ΔΙΑΡΚΕΙΑ ΣΥΜΦΩΝΗΤΙΚΟΥ &emsp; ΕΝΑΡΞΗ: {startWork} &emsp; ΛΗΞΗ: {finishWork}</b></p>
+                    <p><b>ΔΙΑΡΚΕΙΑ ΣΥΜΦΩΝΗΤΙΚΟΥ &emsp; ΕΝΑΡΞΗ: {startWork===null ? "ηη/μμ/εεεε" : startWork} &emsp; ΛΗΞΗ: {finishWork===null ? "ηη/μμ/εεεε" : finishWork}</b></p>
                     <p><b>VOUCHER:</b></p>
                     <div className="voucher">
                         <div className="code">
                             <img className="QRCode" src={QR===null ? defaultQR : QR}/>
                         </div>
                         <div className="info">
-                            <p className="date">{month} {year}</p>
-                            <p className="amount">ΠΟΣΟ: {voucherAmount} €</p>
+                            <p className="date">{month===null ? "ΜΗΝΑΣ" : month} {year===null ? "ΕΤΟΣ" : year}</p>
+                            <p className="amount">ΠΟΣΟ: {voucherAmount===null ? "ΕΕΕ,ΛΛ" : voucherAmount} €</p>
                         </div>
                     </div>
-                    <p>Τσεκάροντας το παρακάτω κουτί, συμφωνείτε πως η/ο επαγγελματίας {professionalFirstName} {professionalLastName} έχει ολοκληρώσει την εργασία της/του για τον μήνα {month} του {year}, και δρομολογείτε την πληρωμή του για αυτόν τον μήνα μέσω του voucher.</p>
-                    <p><b>ΕΠΙΒΑΙΒΕΩΣΗ ΟΛΟΚΛΗΡΩΣΗΣ ΕΡΓΑΣΙΑΣ ΓΙΑ ΤΟΝ ΜΗΝΑ {month} {year}:</b><input type="checkbox" id="completeWrok"/></p>
-                    <p>Αφού τσεκάρετε το προηγούμενο κουτί, το παρακάτω στοιχείο θα εμφανιστεί τσεκαρισμένο όταν η/ο επαγγελματίας {professionalFirstName} {professionalLastName} επιβεβαιώσει ότι έλαβε το voucher για τον μήνα {month} του {year}.</p>
+                    <p>Τσεκάροντας το παρακάτω κουτί, συμφωνείτε πως η/ο επαγγελματίας {professionalFirstName===null ? "Όνομα" : professionalFirstName} {professionalLastName===null ? "Επώνυμο" : professionalFirstName} έχει ολοκληρώσει την εργασία της/του για τον μήνα {month===null ? "ΜΗΝΑΣ" : month} του {year===null ? "ΕΤΟΣ" : year}, και δρομολογείτε την πληρωμή του για αυτόν τον μήνα μέσω του voucher.</p>
+                    <p><b>ΕΠΙΒΑΙΒΕΩΣΗ ΟΛΟΚΛΗΡΩΣΗΣ ΕΡΓΑΣΙΑΣ ΓΙΑ ΤΟΝ ΜΗΝΑ {month===null ? "ΜΗΝΑΣ" : month} {year===null ? "ΕΤΟΣ" : year}:</b><input type="checkbox" id="completeWrok"/></p>
+                    <p>Αφού τσεκάρετε το προηγούμενο κουτί, το παρακάτω στοιχείο θα εμφανιστεί τσεκαρισμένο όταν η/ο επαγγελματίας {professionalFirstName===null ? "Όνομα" : professionalFirstName} {professionalLastName===null ? "Επώνυμο" : professionalFirstName} επιβεβαιώσει ότι έλαβε το voucher για τον μήνα {month===null ? "ΜΗΝΑΣ" : month} του {year===null ? "ΕΤΟΣ" : year}.</p>
                     <div className="confirmation">
-                        <p><b>ΕΠΙΒΑΙΒΕΩΣΗ ΛΗΨΗΣ VOUCHER ΑΠΟ {professionalFirstName} {professionalLastName}: </b></p>
+                        <p><b>ΕΠΙΒΑΙΒΕΩΣΗ ΛΗΨΗΣ VOUCHER ΑΠΟ {professionalFirstName===null ? "Όνομα" : professionalFirstName} {professionalLastName===null ? "Επώνυμο" : professionalFirstName}: </b></p>
                         <img src={voucherRecieved==true ? checkMark : crossMark}/>
                     </div>
                 </div>

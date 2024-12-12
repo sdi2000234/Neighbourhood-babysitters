2354fa91183a319bc68843f35746b726533f9631
sdi2000166 <sdi2000166@di.uoa.gr>
2024-12-12 20:24:14 +0200

    adjustments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css';
+//import './App.css';
 import HomePage from './pages/HomePage/HomePage';
+import ParentNavigation from './components/ParentNavigation';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-
+    <ParentNavigation/>
     <HomePage/> 
-    
+    <Footer/>
     </>
   );
 }

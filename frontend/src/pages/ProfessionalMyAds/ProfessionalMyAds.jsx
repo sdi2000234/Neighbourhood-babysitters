64933490cc31694d35fee9d8cea77573d75738c1
sdi2000166 <sdi2000166@di.uoa.gr>
2024-12-13 15:41:50 +0200

    added My Ads page for professionals

--- frontend/src/pages/ProfessionalMyAds/ProfessionalMyAds.jsx
+++ frontend/src/pages/ProfessionalMyAds/ProfessionalMyAds.jsx
@@ -2,19 +2,25 @@ import React from 'react'
 import './ProfessionalMyAds.css'
 import ProfessionalNavigation from '../../components/ProfessionalNavigation'
 import Footer from '../../components/Footer'
+import AdPreview from '../../components/AdPreview'
+import AdEdit from '../../components/AdEdit'
+import plusGrey from '../../assets/plus_grey.png'
 
-const ProfessionalMyAds = () => {
+function ProfessionalMyAds()
+{
   return (
     <div>
       <ProfessionalNavigation/>
       <div className="breadcrumbs">
         <p>ΟΙ ΑΓΓΕΛΙΕΣ ΜΟΥ</p>
       </div>
-      <div className="createNewAd">
-        <p>Δημιουργία νέας αγγελίας</p>
-      </div>
-      <div className="pageIndex">
-        <p>&lt;&emsp;1&emsp;&gt;</p>
+      <p className="createText"><b>Δημιουργία νέας αγγελίας</b></p>
+      <div className="adPannels">
+        <div className="createNewAd">
+          <button className="newAd"><img alt="plus icon" src={plusGrey}/></button>
+        </div>
+        <AdPreview/>
+        <AdEdit/>
       </div>
       <Footer/>
     </div>

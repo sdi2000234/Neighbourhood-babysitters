4c1d4caec50ab91ccfc687f9d0be53f6af7ca6f0
sdi2000166 <sdi2000166@di.uoa.gr>
2024-12-23 14:23:24 +0200

    adjustments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
+import React from 'react';
+
 import ParentContractPayment from './pages/ParentContractPayment/ParentContractPayment';
-import ParentContractEnd from './pages/ParentContractEnd/ParentContractEnd';
-import ParentContractRenew from './pages/ParentContractEnd/ParentContractRenew';
-import ProfessionalMyAds from './pages/ProfessionalMyAds/ProfessionalMyAds';
-import ProfessionalCreateAd1 from './pages/ProfessionalMyAds/ProfessionalCreateAd1';
-import ProfessionalCreateAd2 from './pages/ProfessionalMyAds/ProfessionalCreateAd2';
-import ProfessionalCreateAd3 from './pages/ProfessionalMyAds/ProfessionalCreateAd3';
-import ProfessionalCreateAd4 from './pages/ProfessionalMyAds/ProfessionalCreateAd4';
+// import ParentContractEnd from './pages/ParentContractEnd/ParentContractEnd';
+// import ParentContractRenew from './pages/ParentContractEnd/ParentContractRenew';
+// import ProfessionalMyAds from './pages/ProfessionalMyAds/ProfessionalMyAds';
+// import ProfessionalCreateAd1 from './pages/ProfessionalMyAds/ProfessionalCreateAd1';
+// import ProfessionalCreateAd2 from './pages/ProfessionalMyAds/ProfessionalCreateAd2';
+// import ProfessionalCreateAd3 from './pages/ProfessionalMyAds/ProfessionalCreateAd3';
+// import ProfessionalCreateAd4 from './pages/ProfessionalMyAds/ProfessionalCreateAd4';
 
 function App()
 {

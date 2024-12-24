9baa391484e6372c8647b71a7f30ee38b611f8d8
ChristinaKok <christinakokk01@gmail.com>
2024-12-24 16:17:14 +0200

    αλλαγη για να μην γινεται αριστερο scroll

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,9 +8,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import RatingParents from './pages/RatingPage/RatingParents';
 // import RatingBabySitters from './pages/RatingPage/RatingBabySitters';
 
-// import ParentAppointment from './pages/Appointments/ParentAppointment.jsx';
+import ParentAppointment from './pages/Appointments/ParentAppointment.jsx';
 // import ParentAppointmentEnd from './pages/Appointments/ParentAppointmentEnd.jsx';
-import ParentAllAppointments from './pages/Appointments/ParentAllAppointments.jsx';
+// import ParentAllAppointments from './pages/Appointments/ParentAllAppointments.jsx';
 
 
 function App() {
@@ -31,11 +31,11 @@ function App() {
         {/* <Route path='/RatingBabySitters' element={<RatingBabySitters />} /> */}
         
         {/* ΔΙΚΟ ΜΟΥ */}
-        {/* <Route path='/ParentAppointment' element={<ParentAppointment babysitterName={babysitterName} userName={userName} userLastName={userLastName} userPhone={userPhone} userEmail={userEmail} date={date} time={time}/>} /> */}
+        <Route path='/ParentAppointment' element={<ParentAppointment babysitterName={babysitterName} userName={userName} userLastName={userLastName} userPhone={userPhone} userEmail={userEmail} date={date} time={time}/>} />
         {/* ΔΙΚΟ ΜΟΥ */}
         {/* <Route path='/ParentAppointmentEnd' element={<ParentAppointmentEnd />} /> */}
         {/* ΔΙΚΟ ΜΟΥ */}
-        <Route path='/ParentAllAppointments' element={<ParentAllAppointments />} />
+        {/* <Route path='/ParentAllAppointments' element={<ParentAllAppointments />} /> */}
 
         
 

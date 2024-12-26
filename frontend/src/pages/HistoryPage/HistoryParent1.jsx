f79f872be647e9389b916cbbee34df3768213255
ChristinaKok <christinakokk01@gmail.com>
2024-12-26 18:49:34 +0200

    ιστορικό γονέων αιτήσεις ολοκληρωμένο

--- frontend/src/pages/HistoryPage/HistoryParent1.jsx
+++ frontend/src/pages/HistoryPage/HistoryParent1.jsx
@@ -11,63 +11,79 @@ function HistoryParent1() {
     { name: 'ΑΙΤΗΣΕΙΣ' },
   ];
 
-  const pic = 'https://hips.hearstapps.com/hmg-prod/images/best-small-dog-breeds-chihuahua-1598967884.jpg?crop=0.449xw:0.842xh;0.245xw,0.0337xh&resize=980:*';
-  const text1 = 'Αίτηση';
-  const text2 = '#014';
-  const text3 = 'Ημερομηνία:';
-  const text4 = '26/10/2024';
-  const text5 = 'Επαγγελματίας:';
-  const text6 = 'Δώρα Τσουφλίδη';
-  const text7 = 'Δ';
-  const text8 = 'Δώρα ΤσουφλίδηΔώρα';
-
+  // Ορίστε το array με τα δεδομένα που θέλεις να εμφανίσεις για κάθε HistoryLine
+  const historyData = [
+    {
+      pic: 'https://hips.hearstapps.com/hmg-prod/images/best-small-dog-breeds-chihuahua-1598967884.jpg?crop=0.449xw:0.842xh;0.245xw,0.0337xh&resize=980:*',
+      text1: 'Αίτηση',
+      text2: '#014',
+      text3: 'Ημερομηνία:',
+      text4: '26/10/2024',
+      text5: 'Επαγγελματίας:',
+      text6: 'Δώρα Τσουφλίδη',
+    },
+    {
+      pic: 'https://hips.hearstapps.com/hmg-prod/images/best-small-dog-breeds-chihuahua-1598967884.jpg?crop=0.449xw:0.842xh;0.245xw,0.0337xh&resize=980:*',
+      text1: 'Αίτηση',
+      text2: '#015',
+      text3: 'Ημερομηνία:',
+      text4: '01/11/2024',
+      text5: 'Επαγγελματίας:',
+      text6: 'Γιάννης Παπαδόπουλος',
+    },
+    {
+      pic: 'https://hips.hearstapps.com/hmg-prod/images/best-small-dog-breeds-chihuahua-1598967884.jpg?crop=0.449xw:0.842xh;0.245xw,0.0337xh&resize=980:*',
+      text1: 'Αίτηση',
+      text2: '#016',
+      text3: 'Ημερομηνία:',
+      text4: '10/11/2024',
+      text5: 'Επαγγελματίας:',
+      text6: 'Μαρία Κωστοπούλου',
+    }
+  ];
 
   return (
     <>
       <ParentNavigation currentNavPage={'parHiReq'} />
-
       <MyBreadcrumbs breadcrumbPages={breadcrumbPages}></MyBreadcrumbs>
 
-      <div
-        className='HistoryParent1Container'
-      >
-        <div>
-          <HistoryLine
-            pic={pic}
-            text1={text1}
-            text2={text2}
-            text3={text3}
-            text4={text4}
-            text5={text5}
-            text6={text6}
-          />
-        </div>
-        <div>
-          <HistoryLine
-            pic={pic}
-            text1={text1}
-            text2={text2}
-            text3={text3}
-            text4={text4}
-            text5={text5}
-            text6={text8}
-          />
-        </div>
-        <div>
-          <HistoryLine
-            pic={pic}
-            text1={text1}
-            text2={text2}
-            text3={text3}
-            text4={text4}
-            text5={text5}
-            text6={text7}
-          />
+      <div className='HistoryParent1Container'>
+        <div className='card1 text-center'>
+          <div className='card-header1'>
+            <ul className='nav1 nav-tabs1 card-header-tabs'>
+              <li className='nav-item1'>
+                <a className='nav-link1 active' href='frontend\src\pages\HistoryPage\HistoryParent1.jsx'>ΑΙΤΗΣΕΙΣ</a>
+              </li>
+              <li className='nav-item1'>
+                <a className='nav-link1' href='frontend\src\pages\HistoryPage\HistoryParent2.jsx'>ΣΥΜΦΩΝΗΤΙΚΑ</a>
+              </li>
+              <li className='nav-item1'>
+                <a className='nav-link1' href='frontend\src\pages\HistoryPage\HistoryParent3.jsx'>ΠΛΗΡΩΜΕΣ</a>
+              </li>
+            </ul>
+          </div>
+          <div className='card-body1'>
+            <div className='HistoryParent1LineContainer'>
+              {historyData.map((data, index) => (
+                <div key={index}>
+                  <HistoryLine
+                    pic={data.pic}
+                    text1={data.text1}
+                    text2={data.text2}
+                    text3={data.text3}
+                    text4={data.text4}
+                    text5={data.text5}
+                    text6={data.text6}
+                  />
+                  <br />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
 
       <Footer />
-
     </>
   );
 }

ce6b47e3aafaff613255439df5ed3a07297707b2
ChristinaKok <christinakokk01@gmail.com>
2024-12-26 20:05:12 +0200

    επαγγελματίες ιστορικο συμφωνητικά και πληρωμές

---
+++ frontend/src/pages/HistoryPage/HistoryProfessional2.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+import HistoryLine from '../../components/HistoryLine';
+import './HistoryProfessional2.css';
+import ProfessionalNavigation from '../../components/ProfessionalNavigation';
+import Footer from '../../components/Footer';
+import MyBreadcrumbs from '../../components/MyBreadcrumbs';
+import { Menu, MenuItem, Button } from '@mui/material';
+import arrow_white from '../../assets/arrow_white.png';
+
+function HistoryProfessional2() {
+  const breadcrumbPages = [
+    { name: 'ΙΣΤΟΡΙΚΟ' },
+    { name: 'ΣΥΜΦΩΝΗΤΙΚΟ' },
+  ];
+
+  // Ορίστε το array με τα δεδομένα που θέλεις να εμφανίσεις για κάθε HistoryLine
+  const historyData = [
+    {
+        pic: 'https://hips.hearstapps.com/hmg-prod/images/best-small-dog-breeds-chihuahua-1598967884.jpg?crop=0.449xw:0.842xh;0.245xw,0.0337xh&resize=980:*',
+        text1: 'Έναρξη:',
+        text2: '01/09/2024',
+        text3: 'Λήξη:',
+        text4: '28/02/2025',
+        text5: 'Γονέας/Κηδεμόνας:',
+        text6: 'Αγγελική Χριστοπούλου',
+    },
+    {
+        pic: 'https://hips.hearstapps.com/hmg-prod/images/best-small-dog-breeds-chihuahua-1598967884.jpg?crop=0.449xw:0.842xh;0.245xw,0.0337xh&resize=980:*',
+        text1: 'Έναρξη:',
+        text2: '01/09/2024',
+        text3: 'Λήξη:',
+        text4: '28/02/2025',
+        text5: 'Γονέας/Κηδεμόνας:',
+        text6: 'Αγγελική Χριστοπούλου',
+      },
+    {
+        pic: 'https://hips.hearstapps.com/hmg-prod/images/best-small-dog-breeds-chihuahua-1598967884.jpg?crop=0.449xw:0.842xh;0.245xw,0.0337xh&resize=980:*',
+        text1: 'Έναρξη:',
+        text2: '01/09/2024',
+        text3: 'Λήξη:',
+        text4: '28/02/2025',
+        text5: 'Γονέας/Κηδεμόνας:',
+        text6: 'Αγγελική Χριστοπούλου',
+    },
+  ];
+
+  // Κατάσταση για το ανοίγμα και το κλείσιμο του dropdown menu
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  // Λειτουργία για το άνοιγμα του dropdown
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  // Λειτουργία για το κλείσιμο του dropdown
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
+
+  return (
+    <>
+      <ProfessionalNavigation currentNavPage={'profHiCon'} />
+      <MyBreadcrumbs breadcrumbPages={breadcrumbPages}></MyBreadcrumbs>
+
+      <div className='HistoryProfessional2Container'>
+        <div className='card2 text-center'>
+          <div className='card-header2'>
+
+            {/* dropdown menu για ταξινόμηση το Material UI  */}
+            <div>
+              <Button 
+                aria-controls="simple-menu" 
+                aria-haspopup="true" 
+                onClick={handleClick}
+                variant="contained" 
+                className='ButtonHistoryProfessional2'
+                endIcon={<img src={arrow_white} alt="arrow" style={{ width: '13px', height: '13px', marginLeft: '8px' }} />}
+              >
+                Ταξινόμηση ανά Ημερομηνίας
+              </Button>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Αύξουσα</MenuItem>
+                <MenuItem onClick={handleClose}>Φθίνουσα</MenuItem>
+              </Menu>
+            </div>
+
+            <ul className='nav2 nav-tabs2 card-header-tabs'>
+              <li className='nav-item2'>
+                <a className='nav-link2' href='http://localhost:3000/HistoryProfessional1'>ΑΙΤΗΣΕΙΣ</a>
+              </li>
+              <li className='nav-item2'>
+                <a className='nav-link2 active' href='http://localhost:3000/HistoryProfessional2'>ΣΥΜΦΩΝΗΤΙΚΑ</a>
+              </li>
+              <li className='nav-item2'>
+                <a className='nav-link2' href='http://localhost:3000/HistoryProfessional3'>ΠΛΗΡΩΜΕΣ</a>
+              </li>
+            </ul>
+          </div>
+          <div className='card-body2'>
+            <div className='HistoryProfessional2LineContainer'>
+              {historyData.map((data, index) => (
+                <div key={index}>
+                  <HistoryLine
+                    pic={data.pic}
+                    text1={data.text1}
+                    text2={data.text2}
+                    text3={data.text3}
+                    text4={data.text4}
+                    text5={data.text5}
+                    text6={data.text6}
+                  />
+                  <br />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
+
+export default HistoryProfessional2;
---
+++ frontend/src/pages/HistoryPage/HistoryProfessional3.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from 'react';
+import HistoryLine from '../../components/HistoryLine';
+import './HistoryProfessional3.css';
+import ProfessionalNavigation from '../../components/ProfessionalNavigation';
+import Footer from '../../components/Footer';
+import MyBreadcrumbs from '../../components/MyBreadcrumbs';
+import { Menu, MenuItem, Button } from '@mui/material';
+import arrow_white from '../../assets/arrow_white.png';
+
+function HistoryProfessional3() {
+  const breadcrumbPages = [
+    { name: 'ΙΣΤΟΡΙΚΟ' },
+    { name: 'ΠΛΗΡΩΜΕΣ' },
+  ];
+
+  // Ορίστε το array με τα δεδομένα που θέλεις να εμφανίσεις για κάθε HistoryLine
+  const historyData = [
+    {
+        pic: 'https://hips.hearstapps.com/hmg-prod/images/best-small-dog-breeds-chihuahua-1598967884.jpg?crop=0.449xw:0.842xh;0.245xw,0.0337xh&resize=980:*',
+        text1: 'Ημερομηνία:',
+        text2: '01/11/2024',
+        text3: 'Ποσό:',
+        text4: '500,00\u20AC ',
+        text5: 'Από:',
+        text6: 'Αγγελική Χριστοπούλου',
+    },
+    {
+        pic: 'https://hips.hearstapps.com/hmg-prod/images/best-small-dog-breeds-chihuahua-1598967884.jpg?crop=0.449xw:0.842xh;0.245xw,0.0337xh&resize=980:*',
+        text1: 'Ημερομηνία:',
+        text2: '01/11/2024',
+        text3: 'Ποσό:',
+        text4: '500,00\u20AC ', 
+        text5: 'Από:',
+        text6: 'Αγγελική Χριστοπούλου',
+    },
+    {
+        pic: 'https://hips.hearstapps.com/hmg-prod/images/best-small-dog-breeds-chihuahua-1598967884.jpg?crop=0.449xw:0.842xh;0.245xw,0.0337xh&resize=980:*',
+        text1: 'Ημερομηνία:',
+        text2: '01/11/2024',
+        text3: 'Ποσό:',
+        text4: '500,00\u20AC ',
+        text5: 'Από:',
+        text6: 'Αγγελική Χριστοπούλου',
+    }
+  ];
+
+  // Κατάσταση για το ανοίγμα και το κλείσιμο του dropdown menu
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  // Λειτουργία για το άνοιγμα του dropdown
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  // Λειτουργία για το κλείσιμο του dropdown
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <ProfessionalNavigation currentNavPage={'profHiPay'} />
+      <MyBreadcrumbs breadcrumbPages={breadcrumbPages}></MyBreadcrumbs>
+
+      <div className='HistoryProfessional3Container'>
+        <div className='card3 text-center'>
+          <div className='card-header3'>
+
+            {/* dropdown menu για ταξινόμηση το Material UI  */}
+            <div>
+              <Button 
+                aria-controls="simple-menu" 
+                aria-haspopup="true" 
+                onClick={handleClick}
+                variant="contained" 
+                className='ButtonHistoryProfessional3'
+                endIcon={<img src={arrow_white} alt="arrow" style={{ width: '13px', height: '13px', marginLeft: '8px' }} />}
+              >
+                Ταξινόμηση ανά Ημερομηνίας
+              </Button>
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Αύξουσα</MenuItem>
+                <MenuItem onClick={handleClose}>Φθίνουσα</MenuItem>
+              </Menu>
+            </div>
+
+            <ul className='nav3 nav-tabs3 card-header-tabs'>
+              <li className='nav-item3'>
+                <a className='nav-link3' href='http://localhost:3000/HistoryProfessional1'>ΑΙΤΗΣΕΙΣ</a>
+              </li>
+              <li className='nav-item3'>
+                <a className='nav-link3' href='http://localhost:3000/HistoryProfessional2'>ΣΥΜΦΩΝΗΤΙΚΑ</a>
+              </li>
+              <li className='nav-item3'>
+                <a className='nav-link3 active' href='http://localhost:3000/HistoryProfessional3'>ΠΛΗΡΩΜΕΣ</a>
+              </li>
+            </ul>
+          </div>
+          <div className='card-body3'>
+            <div className='HistoryProfessional3LineContainer'>
+              {historyData.map((data, index) => (
+                <div key={index}>
+                  <HistoryLine
+                    pic={data.pic}
+                    text1={data.text1}
+                    text2={data.text2}
+                    text3={data.text3}
+                    text4={data.text4}
+                    text5={data.text5}
+                    text6={data.text6}
+                  />
+                  <br />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
+}
+
+export default HistoryProfessional3;

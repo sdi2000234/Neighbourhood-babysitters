7832aa5ca3b7fdf8104df01dd7b90d248befd22c
ChristinaKok <christinakokk01@gmail.com>
2025-01-05 12:59:47 +0200

    επιπλέον έλεγχοι για στοιχεία ραντεβού

--- frontend/src/pages/Appointments/ParentAppointment.jsx
+++ frontend/src/pages/Appointments/ParentAppointment.jsx
@@ -11,24 +11,28 @@ import { Tooltip } from '@mui/material';
 function ParentAppointment({ babysitterName , userName , userLastName , userPhone, userEmail}) {
 
     const navigate = useNavigate();
-    
     const [meetingType, setMeetingType] = useState(''); // Διαχειρίζεται την επιλογή του τύπου συνάντησης
 
     const handleMeetingTypeChange = (event) => {
         setMeetingType(event.target.value); // Ενημέρωση τύπου συνάντησης για radiobuttons 
     };
 
-    const handleCancel = (event) => { //Για button Ακύρωσης 
+    const handleCancel = (event) => { // Για button Ακύρωσης 
         event.preventDefault();  // Αποτρέπει την υποβολή της φόρμας
-        alert("Η φόρμα ακυρώθηκε."); //ΠΟΥ ΝΑ ΠΗΓΑΙΝΟΥΜΕ ΣΤΗΝ ΑΚΥΡΩΣΗ ???
+        alert("Η φόρμα ακυρώθηκε."); 
     };
 
-    const handleSubmit = (event) => { //Για button Αποστολή
+    const handleSubmit = (event) => { // Για button Αποστολή
+        event.preventDefault(); // Αποτρέπει την προεπιλεγμένη υποβολή
+        if (!meetingType) {
+            alert("Παρακαλώ επιλέξτε τύπο συνάντησης.");
+            return;
+        }
         navigate('/ParentAppointmentEnd'); 
     };
 
     const breadcrumbPages = [
-        { name: 'ΠΡΟΣΛΗΨΗ ΕΠΑΓΓΕΛΜΑΤΙΑ'}  //ΙΣΩΣ ΘΕΛΕΙ ΕΞΤΡΑ "ΒΗΜΑΤΑ" ΕΔΩ
+        { name: 'ΠΡΟΣΛΗΨΗ ΕΠΑΓΓΕΛΜΑΤΙΑ'}  // ΙΣΩΣ ΘΕΛΕΙ ΕΞΤΡΑ "ΒΗΜΑΤΑ" ΕΔΩ
     ];
 
     return (
@@ -41,47 +45,48 @@ function ParentAppointment({ babysitterName , userName , userLastName , userPhon
                 <h1>ΚΛΕΙΣΙΜΟ ΡΑΝΤΕΒΟΥ ΜΕ:</h1>
                 <h1> {babysitterName}</h1>
 
-                <form action="">
-
+                <form onSubmit={handleSubmit}>
                     <p className="infoType">Όνομα:</p>
-                    <p className="infoBox">{userName==="" ? "Όνομα" : userName}</p>
+                    <p className="infoBox">{userName === "" ? "Όνομα" : userName}</p>
                     <br/>
                     <p className="infoType">Επώνυμο:</p>
-                    <p className="infoBox">{userLastName==="" ? "Επώνυμο" : userLastName}</p>
+                    <p className="infoBox">{userLastName === "" ? "Επώνυμο" : userLastName}</p>
 
                     <div className="phoneAndEmail">
                         <div style={{ flexGrow: 1 }}>
                             <p className="infoType">Τηλέφωνο:</p>
-                            <p className="infoBox">{userPhone==="" ? "Τηλέφωνο" : userPhone}</p>
+                            <p className="infoBox">{userPhone === "" ? "Τηλέφωνο" : userPhone}</p>
                         </div>
                         <div style={{ flexGrow: 1 }}>
                             <p className="infoType">Ηλεκτρονικό Ταχυδρομίο:</p>
-                            <p className="infoBox">{userEmail==="" ? "Ηλεκτρονικό Ταχυδρομίο" : userEmail}</p>
+                            <p className="infoBox">{userEmail === "" ? "Ηλεκτρονικό Ταχυδρομίο" : userEmail}</p>
                         </div>
                     </div>
 
                     <div className="dateAndtime">
                         <div>
                             <br /><br />
-                            <label htmlFor="date" className="infoType"> <RequiredField label="Ημερομηνία Ραντεβού:" /> </label>
+                            <label htmlFor="date" className="infoType">
+                                <RequiredField label="Ημερομηνία Ραντεβού:" />
+                            </label>
                             <input type="date" id="date" name="date" className="infoBox" placeholder="Ημερομηνία Ραντεβού" required />
                         </div>
                         <div>
                             <br /><br />
-                            <label htmlFor="time" className="infoType"> <RequiredField label="Ώρα Ραντεβού:" /> </label>
+                            <label htmlFor="time" className="infoType">
+                                <RequiredField label="Ώρα Ραντεβού:" />
+                            </label>
                             <input type="time" id="time" name="time" className="infoBox" placeholder="Ώρα Ραντεβού" required />
                         </div>
                     </div>
 
                     <div className="container">
-
                         <div>
                             <br /><br />
                             <RequiredField label="Τύπος Συνάντησης:" />
                         </div>
 
-                        <div class="meeting">
-                            
+                        <div className="meeting">
                             <div>
                                 <label id="radiobutton">
                                     <input type="radio" value="FaceToFace" name="meeting" onChange={handleMeetingTypeChange} required /> 
@@ -95,25 +100,42 @@ function ParentAppointment({ babysitterName , userName , userLastName , userPhon
                                 </label>
                             </div>
                         </div>
-                        <br/>
+                        <br />
                         <br />
 
-                        {/* Δυναμική απόδοση πεδίων βάσει της επιλογής online ή face to face meeting*/}
+                        {/* Δυναμική απόδοση πεδίων βάσει της επιλογής online ή face to face meeting */}
                         {meetingType === "FaceToFace" && (
                             <div>
                                 <label htmlFor="location" className="infoType">Διεύθυνση Ραντεβού:</label>
-                                <input type="text" id="location" name="location" className="infoBox" placeholder="Διεύθυνση , Περιοχή , Τ.Κ." required />
+                                <input 
+                                    type="text" 
+                                    id="location" 
+                                    name="location" 
+                                    className="infoBox" 
+                                    placeholder="Διεύθυνση Αριθμός, Περιοχή , Τ.Κ." 
+                                    pattern="^[^,]+?\s*,\s*[^,]+?\s*,\s*[0-9]{5}$"
+                                    title="Η διεύθυνση πρέπει να έχει τη μορφή 'Διεύθυνση Αριθμός, Περιοχή, Τ.Κ.' π.χ. 'Ερμού 15, Αθήνα, 10563'" 
+                                    required 
+                                />
                             </div>
                         )}
 
-
                         {meetingType === "Online" && (
                             <div>
-                                <label htmlFor="location" className="infoType">Link online συνάντησης:</label>
+                                <label htmlFor="link" className="infoType">Link online συνάντησης:</label>
                                 <Tooltip title="Δημιουργήστε ένα online meeting σε κάποια πλατφόρμα όπως webex,zoom...και συμπληρώστε εδώ τον σύνδεσμο" arrow>
                                     <InfoIcon sx={{ marginLeft: '5px', cursor: 'pointer' }} />
                                 </Tooltip>
-                                <input type="text" id="link" name="link" className="infoBox" placeholder="Link online συνάντησης" required />
+                                <input 
+                                    type="url" 
+                                    id="link" 
+                                    name="link" 
+                                    className="infoBox" 
+                                    placeholder="Link online συνάντησης" 
+                                    pattern="https?://.*" 
+                                    title="Συμπληρώστε έναν έγκυρο σύνδεσμο URL (π.χ., https://example.com)" 
+                                    required 
+                                />
                             </div>
                         )}
 
@@ -122,13 +144,10 @@ function ParentAppointment({ babysitterName , userName , userLastName , userPhon
                             <textarea className="mymessage" name="subject" placeholder="Μήνυμα προς Επαγγελματία.." style={{ height:200 }}></textarea>
                         </div>
 
-                        <button type="submit" className="submitbutton" onClick={handleSubmit}>Αποστολή</button>
+                        <button type="submit" className="submitbutton">Αποστολή</button>
                         <br />
                         <button type="button" className="cancelbutton" onClick={handleCancel}>Ακύρωση</button>
-
-
                     </div>
-
                 </form>
             </div>
             <Footer />
